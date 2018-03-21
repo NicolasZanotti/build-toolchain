1439ad261abcd5c745621946b6f5d11a0f446ab2
Nicolas Zanotti <nicolas@getyourguide.com>
2018-03-21 17:42:18 +0100

    Fix function name

--- build/tasks/info.js
+++ build/tasks/info.js
@@ -2,7 +2,7 @@ const state = require('../state');
 const log = require('fancy-log');
 const wrap = require('ansi-wrap');
 
-module.exports = function cleanTask(gulp, done) {
+module.exports = function infoTask(gulp, done) {
 	log(
 		wrap(32, 39, state.toString())
 	);
